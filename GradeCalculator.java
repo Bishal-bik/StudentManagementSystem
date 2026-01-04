bb5bf1f91a5e6c0532159ae6cb864e938728e4a9
Bishal-bik <bissu.khanal61@gmail.com>
2026-01-04 13:12:25 +0545

    Updated GradeCalculator.java with new feature

---
+++ GradeCalculator.java
@@ -0,0 +1,9 @@
+public class GradeCalculator {
+    public double calculateAverage(int[] marks) {
+        int sum = 0;
+        for (int mark : marks) {
+            sum += mark;
+        }
+        return (double) sum / marks.length;
+    }
+}
